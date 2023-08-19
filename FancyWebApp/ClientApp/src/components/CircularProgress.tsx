3830199ac6e1f1302a2259058b222067ae108f14
Andrea Magnanini <63046209+AndreaMagnanini@users.noreply.github.com>
2023-08-19 02:02:39 +0200

    circular progress

---
+++ FancyWebApp/ClientApp/src/components/CircularProgress.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    container: {
+        position: "relative",
+        width: "48px",
+        height: "48px",
+        borderRadius: "100%",
+        overflow: "hidden",
+        boxShadow: "inset 0px 0px 0px 6px #eeeeee",
+
+    },
+    spinner: {
+        animationName: "$spin",
+        animationDuration: "1s",
+        animationTimingFunction: "linear",
+        animationIterationCount: "infinite",
+        display: "block",
+        width:" 100%",
+        height: "100%",
+        clip: "rect(0, 48px, 48px, 24px)",
+        position: "absolute",
+        borderRadius: "100%",
+        boxShadow: "inset 0px 0px 0px 6px #D41217",
+    },
+    "@keyframes spin": {
+        from: { transform: "rotate(0deg)" },
+        to: { transform: "rotate(360deg)" }
+    }
+
+});
+
+interface CircularProgressProps{
+    className?: string;
+}
+
+const CircularProgress: React.FC<CircularProgressProps> = ({className}) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.container}>
+            <div className={classes.spinner}></div>
+        </div>
+    );
+};
+
+export default CircularProgress;
